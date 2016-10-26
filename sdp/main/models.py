cbccee48b79d0080d1a28dd3d9e049da395a791b
KanakK <kanakkabara@hotmail.com>
2016-10-26 19:44:21 +0800

    Models fix

--- sdp/main/models.py
+++ sdp/main/models.py
@@ -3,3 +3,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+	username = models.CharField(max_length=8)
+	password = models.CharField(max_length=10)
+	name = models.CharField(max_length=40)
