51d0a195f42a909bd430442a0f6cf15e5e97a8d3
KanakK <kanakkabara@hotmail.com>
2016-10-26 20:16:42 +0800

    Models fix

--- sdp/main/models.py
+++ sdp/main/models.py
@@ -10,33 +10,39 @@ class User(models.Model):
 	name = models.CharField(max_length=40)
 	
 class Instructor(models.Model):
-	instructor_id = models.ForeignKey(User)
+	instructor = models.OneToOneField(
+        User,
+        primary_key=True,
+    )
 
 class Category(models.Model):
 	name = models.CharField(max_length=40)
 
 class Course(models.Model):
-	category_id = models.ForeignKey(Category)
-	instructor_id = models.ForeignKey(Instructor)
+	category = models.ForeignKey(Category)
+	instructor = models.ForeignKey(Instructor)
 	name = models.CharField(max_length=40)
 	description = models.TextField()
 	deployed = models.IntegerField(default=0)
 
 class Module(models.Model):
-	course_id = models.ForeignKey(Course)
+	course = models.ForeignKey(Course)
 	name = models.CharField(max_length=40)
 	position = models.IntegerField(default=0)
 
 class Component(models.Model):
-	course_id = models.ForeignKey(Course)
-	module_id = models.ForeignKey(Module)
+	course = models.ForeignKey(Course)
+	module = models.ForeignKey(Module)
 	name = models.CharField(max_length=40)
 	filename = models.CharField(max_length=40)
 	position = models.IntegerField(default=0)
 	
 class Participant(models.Model):
-	participant_id = models.ForeignKey(User)
-	course_id = models.ForeignKey(Course)
+	participant = models.OneToOneField(
+        User,
+        primary_key=True,
+    )
+	course = models.ForeignKey(Course)
 	most_recent_unlocked = models.IntegerField(default=0)
 	
 
